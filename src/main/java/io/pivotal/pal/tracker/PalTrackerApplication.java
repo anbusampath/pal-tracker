f5a114afef370538b3026fb0ed9c835b2cc5863d
Bill Kable <bkable@gmail.com>
2020-02-05 00:41:11 -0600

    Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,13 +1,25 @@
 package io.pivotal.pal.tracker;
 
+import java.util.TimeZone;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(PalTrackerApplication.class, args);
 	}
+	
+	@Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
+    }
 
 }
